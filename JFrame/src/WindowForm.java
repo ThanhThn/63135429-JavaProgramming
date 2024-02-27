439bc62c2e06231f3b93573aaa2b77fc893ed8e1
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2024-02-27 20:56:58 +0700

    Hoàn thành thực hành 0

---
+++ JFrame/src/WindowForm.java
@@ -0,0 +1,31 @@
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+import java.awt.Window;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class WindowForm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f = new JFrame();
+		f.setTitle("Hello World");
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setSize(400, 600);
+		f.setLayout(new FlowLayout());
+		//Create Button
+		JButton btn1 = new JButton("Click Me");
+		btn1.addActionListener();
+		JButton btn2 = new JButton("Click Here");
+		
+		f.add(btn2);
+		f.add(btn1);
+		f.show();
+	}
+
+}
